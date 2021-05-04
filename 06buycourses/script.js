675d24171ea6491a08418c2498abfe91411d8e86
Parul Kohli <45730946+Parul1999@users.noreply.github.com>
2021-05-04 21:00:30 +0530

    2-way binding , if-else ,compute and method

---
+++ 06buycourses/script.js
@@ -0,0 +1,60 @@
+
+const App={
+    data(){
+        return{
+            holdMyItem:[],
+            courseList :[
+                {
+                    id:"lco1",
+                    courseName :"Django Full Stack",
+                    subTitle:"Learn Django woith postgres",
+                    price:199,
+                    courseImage:"django.png",
+               },
+                {
+                    id:"lco2",
+                    courseName :"Interview Preparation",
+                    subTitle:"Crack the FAANG interview",
+                    price:1999,
+                    courseImage:"interview.png",
+            
+                },
+                {
+                    id:"lco3",
+                    courseName :"React Native",
+                    subTitle:"Build Mobile Apps",
+                    price:299,
+                    courseImage:"rn.png",
+        
+                }]
+        }
+    },
+    // Computed and methods store functions
+    // The functions defined in computed could not have parameters bt functions in methods couldd have
+    computed:{
+         finalBill(){
+            //  (course,accumulator) would give an error
+            return this.holdMyItem.reduce((accumulator,course)=>Number(course.price)+accumulator,0) 
+         }
+    },
+    //computed is not actually a function , it the value
+    //when the value in holdMyItem changes , computed method wil re-run
+    //methods doesnt rerun automatically
+    methods:{
+       addToHolder(course)
+       {
+           //if condition if remove from cart wa'snt there
+           if(this.holdMyItem.indexOf(course)==-1)
+           {
+           this.holdMyItem.push(course);
+           }
+       },
+       removeFromHolder(course)
+       {
+           this.holdMyItem.pop(course)
+       }
+    }
+}
+//we dont use => in functions cuz we use this too, so it can create a mess
+
+Vue.createApp(App).mount('#vapp')
