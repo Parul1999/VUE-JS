7d0ccae81239612bf224127369f7114e4ed52ca7
Parul Kohli <45730946+Parul1999@users.noreply.github.com>
2021-05-07 02:20:36 +0530

    modified-rating-app

--- ratingapp/src/App.vue
+++ ratingapp/src/App.vue
@@ -1,40 +1,32 @@
 <template>
 <div class="container">
-<div class="card card-body text-center p-5 mt-5 border-primary">
+<div class="card card-body text-center p-4 mt-5  border-primary">
 <div>
+<h5 class="mt-1 mb-4">Would You like to give a Rating ?</h5>
 <i 
-class="fa fa-star text-warning fa-6x"
-@click="setRating(1)"
-></i><i 
-class="fa fa-star text-warning fa-6x"
-@click="setRating(2)"
-></i><i 
-class="fa fa-star text-warning fa-6x"
-@click="setRating(3)"
-></i><i 
-class="fa fa-star text-warning fa-6x"
-@click="setRating(4)"
-></i><i 
-class="fa fa-star text-warning fa-6x"
-@click="setRating(5)"
+v-for="(star,index) in maxStars"
+:class="index>rating?unselectedColor:selectedColor"
+@click="setRating(index)"
+
+:key="star"
 ></i>
 </div>
 
-<h1 v-if="rating==1" class="text-success mt-4">
+<h4 v-if="rating==0" class="text-success mt-4">
 Sorry to hear that rating😌
-</h1>
-<h1 v-if="rating==2" class="text-success mt-4">
+</h4>
+<h4 v-if="rating==1" class="text-success mt-4">
 We will Improve!🙂
-</h1>
-<h1 v-if="rating==3" class="text-success mt-4">
+</h4>
+<h4 v-if="rating==2" class="text-success mt-4">
 Hope next time we will get more!!
-</h1>
-<h1 v-if="rating==4" class="text-success mt-4">
+</h4>
+<h4 v-if="rating==3" class="text-success mt-4">
 Almost there to 5👍
-</h1>
-<h1 v-if="rating==5" class="text-success mt-4">
-So happy , to make you happy
-</h1>
+</h4>
+<h4 v-if="rating==4" class="text-success mt-4">
+Thank you so much
+</h4>
 </div>
 </div>
 </template>
@@ -45,7 +37,10 @@ export default {
   name: 'App',
   data(){
     return{
-      rating:null
+      rating:-1,
+      maxStars:5,
+      selectedColor:"fa fa-star text-warning fa-3x",
+      unselectedColor:"fa fa-star fa-3x"
     }
   },
   methods:{
