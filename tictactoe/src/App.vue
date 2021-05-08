21a35c1275256e20b7cf9558c6b34f381def5056
Parul Kohli <45730946+Parul1999@users.noreply.github.com>
2021-05-08 23:48:55 +0530

    tictactoe

---
+++ tictactoe/src/App.vue
@@ -0,0 +1,132 @@
+<template>
+<p>Icon</p>
+</template>
+
+<script>
+import Icon from "./components/Icon.vue"
+import Wal from "sweetalert2/dist/sweetalert2.js"
+
+export default {
+  name: 'App',
+  components:{Icon},
+  data(){
+    return{
+      winMessage:"",
+      isCross : true,
+      itemArray: new Array(9).fill("empty")
+    }
+  },
+  watch:{
+    winMessage:function(message)
+    {
+      if(message)
+      {
+        //TODO:call a method to display popup
+        this.showDialog();
+      }
+    }
+  },
+  methods:{
+    showDialog:{
+      Swal.fire({
+        icon:"info",
+        title:"Game Over",
+        text: `${this.winMessage}`
+      })
+    }
+  }
+  handleClick(itemNumber)
+  {
+    if(this.winMessage)
+    {
+      return this.showDialog()
+    }
+    if(this.itemArray[itemNumber]==="empty")
+    {
+      this.itemArray[itemNumber]=this.Cross?"cross":"circle";
+      this.isCross = !this.isCross
+    }
+    else
+    {
+      return Swal.fire("Hey ! Don't act smart")
+    }
+    this.checkIsWinner()
+  },
+  checkIsWinner(){
+    //checking winner of the Game
+    if (
+        this.itemArray[0] === this.itemArray[1] &&
+        this.itemArray[0] === this.itemArray[2] &&
+        this.itemArray[0] !== "empty"
+      ) {
+        this.winMessage = `${this.itemArray[0]} won`;
+      } else if (
+        this.itemArray[3] !== "empty" &&
+        this.itemArray[3] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[5]
+      ) {
+        this.winMessage = `${this.itemArray[3]} won`;
+      } else if (
+        this.itemArray[6] !== "empty" &&
+        this.itemArray[6] === this.itemArray[7] &&
+        this.itemArray[7] === this.itemArray[8]
+      ) {
+        this.winMessage = `${this.itemArray[6]} won`;
+      } else if (
+        this.itemArray[0] !== "empty" &&
+        this.itemArray[0] === this.itemArray[3] &&
+        this.itemArray[3] === this.itemArray[6]
+      ) {
+        this.winMessage = `${this.itemArray[0]} won`;
+      } else if (
+        this.itemArray[1] !== "empty" &&
+        this.itemArray[1] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[7]
+      ) {
+        this.winMessage = `${this.itemArray[1]} won`;
+      } else if (
+        this.itemArray[2] !== "empty" &&
+        this.itemArray[2] === this.itemArray[5] &&
+        this.itemArray[5] === this.itemArray[8]
+      ) {
+        this.winMessage = `${this.itemArray[2]} won`;
+      } else if (
+        this.itemArray[0] !== "empty" &&
+        this.itemArray[0] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[8]
+      ) {
+        this.winMessage = `${this.itemArray[0]} won`;
+      } else if (
+        this.itemArray[2] !== "empty" &&
+        this.itemArray[2] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[6]
+      ) {
+        this.winMessage = `${this.itemArray[2]} won`;
+      }
+  },
+  reloadGame(){
+    this.winMessage="";
+    this.isCross=true;
+    itemArray: new Array(9).fill("empty")
+  }
+}
+</script>
+
+
+++++++++++++++++++  app style  +++++++++++++++++++++++++
+<style>
+main {
+  height: 100vh;
+}
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 5px;
+}
+
+.box {
+  height: 150px;
+}
+</style>
+++++++++++++++++++  app style  +++++++++++++++++++++++++
+
---
+++ tictactoe/src/components/Icon.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+<i class="fa fa-circle fa-4x zoomIn text-primary" 
+v-if="iconname==='circle'"></i>
+<i class="fa fa-times fa-4x zoomIn text-info" 
+v-else-if="iconname==='cross'"/></i>
+<img src="../assets/logo.png" class="img-fluid-zoomIn"
+v-else-if="iconname==='empty'"
+/>
+</div>
+</template>
+
+//put required as a condition , as we havent handled  //the case user enter anything except , circle,       //cross, empty
+<script>
+export default{
+    props:{
+        iconname:{
+            type:String,
+            required:true
+        }
+    }
+    name:'Icon'
+}
+
+</script>
+
+++++++++++++++++++  component style  +++++++++++++++++++++++++
+
+<style>
+img {
+  width: 80px;
+}
+
+@keyframes zoomIn {
+  from {
+    opacity: 0;
+    transform: scale3d(0.3, 0.3, 0.3);
+  }
+
+  50% {
+    opacity: 1;
+  }
+}
+
+.zoomIn {
+  animation-name: zoomIn;
+  animation-duration: 1s;
+}
+</style>
+
+++++++++++++++++++  component style  +++++++++++++++++++++++++
