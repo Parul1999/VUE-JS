ff3b0f257bcbabbcbf2f6cebec183d597b47e395
Parul Kohli <45730946+Parul1999@users.noreply.github.com>
2021-05-10 20:45:34 +0530

    updated

--- tictactoe/src/main.js
+++ tictactoe/src/main.js
@@ -1,13 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-//sweetalert configurations
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
-
-// createApp(App).mount('#app')
-//instead of this we are writting as written in sweetalert docs
-
-const app = createApp(App)
-app.use(VueSweetalert2)
-app.mount('#app')
+ createApp(App).mount('#app')
