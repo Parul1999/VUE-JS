6bb617c4b6e60c24ebd1fa7d0e6200c9db517866
Parul Kohli <45730946+Parul1999@users.noreply.github.com>
2021-05-12 22:15:37 +0530

    main.js

--- tictactoe/src/main.js
+++ tictactoe/src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 
  createApp(App).mount('#app')
 
-// const app = createApp(app)
- //app.use()
-// app.mount('#app')
+ const app = createApp(app)
+ app.use()
+ app.mount('#app')
