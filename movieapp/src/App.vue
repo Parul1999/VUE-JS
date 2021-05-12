f1f00e0b3b820f85b1dba6b77fa33fc6346ae317
Parul Kohli <45730946+Parul1999@users.noreply.github.com>
2021-05-13 03:25:29 +0530

    movieapp basic structure

---
+++ movieapp/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<h2>Movie App</h2>
+</template>
+<script>
+export default {
+  name:'App'
+}
+</script>
+<style>
+* {
+  padding: 0;
+  outline: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+main {
+  background-image: url("./assets/bgc.jpg");
+  background-position: center;
+  background-size: cover;
+  min-height: 100vh;
+}
+</style>
