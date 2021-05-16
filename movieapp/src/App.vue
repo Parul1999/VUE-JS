d7ce46bc42b9407a8e5876498dd94388f4531110
Parul Kohli <45730946+Parul1999@users.noreply.github.com>
2021-05-16 20:39:40 +0530

    locaalstorage-movieapp

--- movieapp/src/App.vue
+++ movieapp/src/App.vue
@@ -1,16 +1,60 @@
 <template>
 <main>
   <Header title="Movie App"></Header>
-<h2>Movie App</h2>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="offset-md-3 col-lg-6 p-2">
+        <div class="card card-body mb-2">
+          <Form :addMovie="addMovie"></Form>
+        </div>
+        <movie-list :movies="movies" :deleteMovie="deleteMovie"></movie-list>
+      </div>
+    </div>
+  </div>
 </main>
 </template>
 <script>
 import Header from './layout/Header'
+import Form from './components/MovieForm'
+import MovieList from './components/MovieList'
+
 export default {
   name:'App',
   components:{
-    Header
+    Header,
+    Form,
+    MovieList
+    },
+  data(){
+    return{
+      movies:[]
     }
+  },
+  methods: {
+    addMovie: function(movie)
+    {
+      this.movies.push(movie);
+      localStorage.setItem("@movies",JSON.stringify(this.movies))
+    },
+    deleteMovie: function(id)
+    {
+      const index = this.movies.findIndex(movie => movie.id===id);
+      this.movies.splice(index,1);
+      localStorage.setItem("@movies",JSON.stringify(this.movies))
+    }
+  },
+    mounted() {
+      if(localStorage.getItem("@movies"))
+      {
+        try{
+            this.movies = JSON.parse(localStorage.getItem("@movies"))
+        }
+        catch(e)
+        {
+          localStorage.removeItem("@movies")
+        }
+      }
+    },
 }
 </script>
 <style>
---
+++ movieapp/src/components/MovieForm.vue
@@ -0,0 +1,51 @@
+<template>
+<!-- In script we need not , provide preventdefault separately in script tag , but can write function directly @submit.prevent will prevent default action and if we equate it to some function , that function will run instead of default function -->
+  <form @submit.prevent="onSubmit" class="text-center">
+      <input 
+      class="form-control mb-2"
+      placeholder="Movie Name"
+      name="movieName"
+      v-model="movieName"
+      type="text"/>
+       <input 
+      class="form-control mb-2"
+      placeholder="Movie URL"
+      name="url"
+      v-model="url"
+      type="text"/>
+      <button type="submit" class="btn btn-info btn-block">Add Movie</button>
+  </form>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid';
+export default {
+name: 'Form',
+props:{
+    addMovie: Function,
+},
+data(){
+    return{
+        movieName:"",
+        url:""
+    }
+},
+methods: {
+    onSubmit:function(){
+        // addmovie , where it's called can be stored to local db , sql ec
+        this.addMovie({
+            name:this.movieName,
+            url:this.url,
+            id:uuidv4(),
+            date:Date.now()
+        });
+        this.movieName="";
+        this.url="";
+    }
+},
+}
+</script>
+
+<style>
+
+</style>
