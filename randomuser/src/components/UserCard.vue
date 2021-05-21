34692c8988d2d1b9c3dfe6c0427ad5412eda33b9
Parul Kohli <45730946+Parul1999@users.noreply.github.com>
2021-05-21 23:07:17 +0530

    random-user-api

--- randomuser/src/components/UserCard.vue
+++ randomuser/src/components/UserCard.vue
@@ -1,8 +1,52 @@
 <template>
+    <div class="card border-primary">
+        <div class="card-body text-center">
+            <!-- user.picture?.large means if picture.large exists then the source will be this -->
+            <img 
+            :src="user.picture?.large"
+            height="150"
+            width="150"
+            class="rounded-circle img-thumbnail border-danger zoomIn userImg"
+            >
+            <div class="card-title">
+                <h1 class="text-primary">
+                    <span class="pl-2">{{user.name?.title}}</span>
+                    <span class="pl-2">{{user.name?.first}}</span>
+                    <span class="pl-2">{{user.name?.last}}</span>
+                </h1>
+                <div>
+                   <i class="fas fa-envelope"> </i>
+                    {{user.email}}
+                </div>
+                <div>
+                   <i class="fas fa-map-marker-alt"></i>
+                    {{user.location?.city}}
+                    {{user.location?.state}}
+                    {{user.location?.country}} 
+                </div>
+                 <div>
+                   <i class="fas fa-phone"> </i>
+                    {{user.cell}}
+                </div>
+            </div>
+            <div class="text-secondary">
+                <h6>
+               Registered on : {{user.registered?.date}}
+                </h6>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+export default {
+    name: "UserCard",
+    props: ["user"]
+}
 </script>
 
-<style>
+<style >
+.userImg{
+ margin-top: -6vw
+}
 </style>
