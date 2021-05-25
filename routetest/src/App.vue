150b0a0fa146062e3adb4ac6f89bb5c11925d8d5
Parul Kohli <45730946+Parul1999@users.noreply.github.com>
2021-05-25 23:55:59 +0530

    router

--- routetest/src/App.vue
+++ routetest/src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link :to="{path:'/account/12'}">Account</router-link>
+      <router-view/>
   </div>
-  <router-view/>
+
+
 </template>
+
+<script>
+export default{
+data(){
+  return{
+  course:{
+name:"React",
+Id:1224
+  }
+  }
+}
+}
+</script>
+
 
 <style>
 #app {
---
+++ routetest/src/views/Account.vue
@@ -0,0 +1,12 @@
+<template>
+<main>
+    <h1>Hello , This is Account</h1>
+    <h1> {{ $route.params }}</h1>
+    </main>
+</template>
+
+<script>
+export default {
+    name:"Account"
+}
+</script>
